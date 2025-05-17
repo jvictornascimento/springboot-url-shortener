a4b6d12a93d13afac130dbddfb9b412631149a8a
victojoao <victojoao789@gmail.comgit config --global user.email victojoao789@gmail.com[200~git config --global user.email~git config --global user.email victojoao789@gmail.com>
2025-05-17 01:52:00 -0300

    Created project

---
+++ src/test/java/com/jvictornascimento/urtshortener/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.urtshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
